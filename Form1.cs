7e4a44a216c14728c7114cf5dd12257bd97c35e8
Vemulakonda Rajesh <vemulakonda.rajesh@optum.com>
2020-04-17 16:03:37 +0530

    First Commit

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SamplExeApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            byte[] exeBytes = Properties.Resources.VSCodeSetup_x64_1_40_0;
+            string exeToRun = Path.Combine(Path.GetTempPath(), "VSCodeSetup_x64_1_40_0.exe");
+
+            using (FileStream exeFile = new FileStream(exeToRun, FileMode.Open))
+                exeFile.Write(exeBytes, 0, exeBytes.Length);
+
+            Process.Start(exeToRun);
+
+        }
+    }
+}
